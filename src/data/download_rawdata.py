da0806289df3203c2c2f692be432bc2cb86c16fa
Keita Watanabe <keitaw09@gmail.com>
2020-01-13 17:46:17 +0900

    Add a missing function call

--- src/data/download_rawdata.py
+++ src/data/download_rawdata.py
@@ -21,4 +21,5 @@ def main():
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
+    main()
 
