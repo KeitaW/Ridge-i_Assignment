699a654c91946c6cb4a107e8434fc84d42967531
Keita Watanabe <keitaw09@gmail.com>
2020-01-26 10:55:41 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM tensorflow/tensorflow:latest-gpu-py3-jupyter
-
 LABEL Description="Tensorflow 2.0 + Jupyter notebook with additonal packages" Version="1.0"
 
 COPY ./requirements.txt .
 COPY ./setup.py .
 COPY ./src .
-RUN pip install -r requirements.txt
+RUN pip3 install -r requirements.txt
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,8 +2,6 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-
-
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
@@ -13,8 +11,7 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-
-
+    
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
